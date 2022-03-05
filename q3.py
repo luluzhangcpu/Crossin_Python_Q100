0462abc64342b2bed63786049438c810d8774bf6
yougebuding <42401339+yougebuding@users.noreply.github.com>
2022-03-06 01:33:49 +0800

    Update q3.py

--- q3.py
+++ q3.py
@@ -8,3 +8,12 @@ for i in range(1,len(l1)+1):
 l2 = l1[::-1]
 for i in l2:
     print(i)
+
+# 第三种做法：reversed 和range 函数类似，生成的是迭代器，不直接返回list表
+for i in reversed(l1):
+    print(i)
+
+# 第四种做法:
+l1.reverse() # 直接倒序，返回至自身
+for i in l1:
+    print(i)
