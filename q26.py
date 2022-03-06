67ce56f4a5bd249fc0ed17fb43fbbc21a9fabf66
yougebuding <42401339+yougebuding@users.noreply.github.com>
2022-03-06 19:17:39 +0800

    Create q26.py

---
+++ q26.py
@@ -0,0 +1,11 @@
+# Q26: 统计 1到100 内质数之和
+
+l1 = [1]
+for i in range(2,101):
+    cnt = 0
+    for j in range(1,i + 1):
+        if i % j == 0:
+            cnt += 1
+    if cnt == 2:
+        l1.append(i)
+print(sum(l1))
