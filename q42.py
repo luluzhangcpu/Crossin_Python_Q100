89f9fd54f47ea942b9560ad9d61c97ca9ca88173
yougebuding <42401339+yougebuding@users.noreply.github.com>
2022-03-06 22:56:03 +0800

    Update q42.py

--- q42.py
+++ q42.py
@@ -1,5 +1,6 @@
 # Q42: 请你帮生成200个优惠券号码。优惠码的字符由26个英文字符（大小写）组成，每个优惠码有8位
 from random import choice
+import string
 l1 = [chr(i) for i in range(65,91)]
 l2 = [chr(i) for i in range(97,123)]
 l3 = l1 + l2
@@ -9,3 +10,5 @@ for i in range(200):
         a = choice(l3)
         l4.append(a)
     print(''.join(l4))
+
+print(list(string.ascii_letters)) # 另一种生成'a-zA-Z'序列
