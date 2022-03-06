3a61dab26162de077d6ff7d703dfbe8104a80e2b
yougebuding <42401339+yougebuding@users.noreply.github.com>
2022-03-07 00:11:31 +0800

    Create q47.py

---
+++ q47.py
@@ -0,0 +1,11 @@
+# Q47: 计算数字列表被最大数与最小数之差
+# 第一种做法
+from functools import reduce
+a = reduce(lambda x,y:x if x >= y else y,[2,4,1,2])
+b = reduce(lambda x,y:x if x <= y else y,[2,4,1,2])
+print(a-b)
+
+# 第二种做法
+max1 = max([2,4,1,2])
+min1 = min([2,4,1,2])
+print(max1 - min1)
