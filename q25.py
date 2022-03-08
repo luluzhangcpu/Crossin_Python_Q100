984ba667b035dac12a182aceaaeca17fd1f52a0e
yougebuding <42401339+yougebuding@users.noreply.github.com>
2022-03-08 22:15:40 +0800

    Update q25.py

--- q25.py
+++ q25.py
@@ -1,10 +1,16 @@
-# Q25: 判断输入数字是否为质数
-cnt = 0
+# Q25: 判断输入数字是否为质数问题】
+# 一个大于1的自然数，除了1和它本身外，不能被其他自然数（质数）整除（2, 3, 5, 7等）
+# 换句话说就是该数除了1和它本身以外不再有其他的因数.
 n = int(input('请输入一个整数:'))
-for i in range(1,n + 1):
-    if n % i == 0:
-        cnt += 1
-if cnt == 1 or cnt == 2:
+a = n
+if n == 1:
+    print('%d 不是质数' % n)
+elif n == 2:
     print('%d 是质数' % n)
 else:
-    print('%d 不是质数' % n)
+    for i in range(2,a):
+        if n % i == 0:
+            print('%d 不是质数' % n)
+            break
+    else:
+        print('%d 是质数' % n)
